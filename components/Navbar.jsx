ecdcad7de1093fa84809439ddff5115c0cf751d0
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2022-06-13 23:37:38 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Link href='/'>
           <Image
-            src='../public/assets/navLogo.png'
+            src='../public/assets/navLogo.PNG'
             alt='/'
             width='205'
             height='100'

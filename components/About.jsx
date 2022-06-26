75e88217aafc7269332cbf211efbb0187ba2a93b
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2022-06-26 20:09:23 +0530

    Update About.jsx
    black theme

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#016170]'>About</p>
+          <p className='uppercase text-xl tracking-widest text-[#ffffff]'>About</p>
           <h2 className='py-4'>Who Am I</h2>
           <p className='py-2 text-white'>
           I have spent the last 3 years in the company called Nielsen working as a
@@ -25,7 +25,7 @@ const About = () => {
           </p>
           <Link href='/#projects'><p className='py-2 text-white underline cursor-pointer'>Check out some of my latest projects.</p></Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 '>
+        <div className='w-full h-auto m-auto shadow-xl  rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 bg-[#303030]'>
             <img className='rounded-xl' src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="/" width='100%' height='100%'/>
         </div>
       </div>

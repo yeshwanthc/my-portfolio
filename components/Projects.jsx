58ce57228244cd489c7614f49d87872f1a0d11f8
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2022-06-26 20:10:57 +0530

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
   return (
     <div id='projects' className='w-full pb-10'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#016170]'>
+        <p className='text-xl tracking-widest uppercase text-[#ffffff]'>
           Projects
         </p>
         <h2 className='py-4'>Websites I Built</h2>

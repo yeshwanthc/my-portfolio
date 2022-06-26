9671c7ba6e11648a39bfe81a2262cb676ab49d0e
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2022-06-26 20:10:43 +0530

    Update ProjectItem.jsx

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const ProjectItem = ({title, backgroundImg, projectUrl}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl bg-white rounded-xl  group hover:bg-gradient-to-r from-[#c2ddda] to-[#00e0c6] '>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl bg-white rounded-xl  group hover:bg-gradient-to-r from-[#303030] to-[#303030] '>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>Frontend</p>
         <Link href={projectUrl} target='_blank'>
-             <p className='text-center py-4 rounded-md bg-[#016170] text-white font-bold text-lg cursor-pointer'>More Info</p>
+             <p className='text-center py-4 rounded-md bg-[#303030] text-white font-bold text-lg cursor-pointer'>More Info</p>
         </Link>
     </div>
  </div>
